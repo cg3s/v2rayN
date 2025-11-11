a87ea3ca1ea80fa01c88ca6bd44d4c8c25bb31cf
Koll Zhu <koll.zhu@gmail.com>
2025-11-12 07:49:16 +0800

    fix code bug

--- v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
+++ v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
@@ -314,17 +314,10 @@ public partial class MainWindow : WindowBase<MainWindowViewModel>
         }
     }
 
-<<<<<<< HEAD
-    //private void menuPromotion_Click(object? sender, RoutedEventArgs e)
-    //{
-    //    ProcUtils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
-    //}
-=======
-    private void MenuPromotion_Click(object? sender, RoutedEventArgs e)
-    {
-        ProcUtils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
-    }
->>>>>>> upstream/master
+    // private void MenuPromotion_Click(object? sender, RoutedEventArgs e)
+    // {
+    //     ProcUtils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
+    // }
 
     private void MenuSettingsSetUWP_Click(object? sender, RoutedEventArgs e)
     {
